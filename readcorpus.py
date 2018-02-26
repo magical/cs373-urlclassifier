8dd0bdf7a3e52870dc11bf58f4e9c2d031fe695d
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2018-02-26 10:43:12 -0800

    replace getopt with argparse

--- readcorpus.py
+++ readcorpus.py
@@ -2,40 +2,25 @@
 
 import json
 import sys
-import getopt
+import argparse
 import os
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file")
+    args = parser.parse_args()
 
-def usage():
-    print("Usage: %s --file=[filename]" % sys.argv[0])
-    sys.exit()
+    if args.file == "":
+        parser.print_usage()
+        return
 
-
-def main(argv):
-
-    file = ''
-
-    myopts, args = getopt.getopt(sys.argv[1:], "", ["file="])
-
-    for o, a in myopts:
-        if o in ('-f, --file'):
-            file = a
-        else:
-            usage()
-
-    if len(file) == 0:
-        usage()
-
-    corpus = open(file)
-    urldata = json.load(corpus, encoding="latin1")
+    with open(args.file) as corpus:
+        urldata = json.load(corpus, encoding="latin1")
 
     for record in urldata:
 
         # Do something with the URL record data...
         print record["domain_age_days"]
 
-    corpus.close()
-
-
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
