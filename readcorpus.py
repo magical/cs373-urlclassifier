bace92e7959b81171dfdc0bbce3ad535d794e469
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2018-02-26 10:37:49 -0800

    run autopep8

--- readcorpus.py
+++ readcorpus.py
@@ -1,35 +1,41 @@
 #!/usr/bin/python
 
-import json, sys, getopt, os
+import json
+import sys
+import getopt
+import os
+
 
 def usage():
-  print("Usage: %s --file=[filename]" % sys.argv[0])
-  sys.exit()
+    print("Usage: %s --file=[filename]" % sys.argv[0])
+    sys.exit()
+
 
 def main(argv):
 
-  file=''
- 
-  myopts, args = getopt.getopt(sys.argv[1:], "", ["file="])
- 
-  for o, a in myopts:
-    if o in ('-f, --file'):
-      file=a
-    else:
-      usage()
+    file = ''
 
-  if len(file) == 0:
-    usage()
- 
-  corpus = open(file)
-  urldata = json.load(corpus, encoding="latin1")
+    myopts, args = getopt.getopt(sys.argv[1:], "", ["file="])
 
-  for record in urldata:
- 
-    # Do something with the URL record data...
-    print record["domain_age_days"]
+    for o, a in myopts:
+        if o in ('-f, --file'):
+            file = a
+        else:
+            usage()
 
-  corpus.close()
+    if len(file) == 0:
+        usage()
+
+    corpus = open(file)
+    urldata = json.load(corpus, encoding="latin1")
+
+    for record in urldata:
+
+        # Do something with the URL record data...
+        print record["domain_age_days"]
+
+    corpus.close()
+
 
 if __name__ == "__main__":
-  main(sys.argv[1:])
+    main(sys.argv[1:])
