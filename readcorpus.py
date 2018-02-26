f943c74ff983c110cc6e4dcc77020648bc20ee56
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2018-02-26 11:59:02 -0800

    add some basic rules

--- readcorpus.py
+++ readcorpus.py
@@ -24,18 +24,42 @@ def main():
 
 def ismalicious(record):
     """classify a single url as either malicious or not malicious"""
-    # Do something with the URL record data...
-    #print record["domain_age_days"], record["malicious_url"]
-    return True
+    score = 0
+    # old domains are likely benign
+    # newly-registered domains are likely malicious
+    if int(record["domain_age_days"]) < 10:
+        score += 1
+    if int(record["domain_age_days"]) > 365:
+        score -= 1
+
+    # valid domains shouldn't have a tld in a subdomain
+    # eg. eu.battle.net.blizzardentertainmentfreeofactivitiese.com
+    if any(tld in record['domain_tokens'][:-1] for tld in ("com", "net", "org")):
+        # .com.cn is valid
+        if not record['host'].endswith(".com.cn"):
+            score += 2
+
+    # www is a good sign
+    if "www" in record['domain_tokens'][:1]:
+        score -= 1
+
+    return score >= 1
 
 def measure(urldata):
     """measure our false positive and false negative rate"""
     matrix = [[0, 0], [0, 0]]
+    false_positives = []
     for record in urldata:
-        prediction = ismalicious(record)
-        actual = record['malicious_url'] == 1
+        prediction = bool(ismalicious(record))
+        actual = bool(record['malicious_url'])
+
+        if prediction == True and actual == False:
+            false_positives.append(record)
 
         matrix[actual][prediction] += 1
+
+    for record in false_positives[:20]:
+        print("false positive:", record['url'])
 
     print("true positives:", matrix[1][1])
     print("true negatives:", matrix[0][0])
@@ -44,6 +68,7 @@ def measure(urldata):
 
     total = matrix[0][0] + matrix[0][1] + matrix[1][0] + matrix[1][1]
     print("accuracy: {:%}".format((matrix[1][1] + matrix[0][0]) / total))
+
 
 if __name__ == "__main__":
     main()
